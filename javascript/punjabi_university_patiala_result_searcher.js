9a505e5fcd457f92330f962db60a28e435d111ac
Navpreet Devpuri <navpreetdevpuri@gmail.com>
2020-10-21 20:41:48 +0530

    Update punjabi_university_patiala_result_searcher.js

--- javascript/punjabi_university_patiala_result_searcher.js
+++ javascript/punjabi_university_patiala_result_searcher.js
@@ -14,28 +14,37 @@ for (i = 0; i < a.length - 5; ++i) {
 	subn = a[i].innerText.split("\n")[0].toLowerCase();
 	yearn = a[i].querySelector("sup").innerText.toLowerCase();
 	if (sem == "") {
-		is_year = yearn == ("- dec-" + year_of_exam) || yearn == ("- may-" + year_of_exam);
+		is_year = yearn == ("- dec-" + year_of_exam) || 
+		yearn == ("- may-" + year_of_exam);
 	} else {
-		temp = ((sem == "i" || sem == "iii" || sem == "v") ? "- dec-" : "- may-") + year_of_exam;
+		temp = ((sem == "i" || sem == "iii" || sem == "v") ? 
+		"- dec-" : 
+		"- may-") + year_of_exam;
 		is_year = yearn == temp;
 	}
 	if (is_year) {
 		if (subn.indexOf(course) != -1) {
 			if (sem != "") {
-				if (subn.indexOf("sem-" + sem) != -1 || subn.indexOf("sem - " + sem) != -1 || subn.indexOf("semester-" + sem) != -1 || subn.indexOf("semester - " + sem) != -1)
+				if (subn.indexOf("sem-" + sem) != -1 || 
+				subn.indexOf("sem - " + sem) != -1 || 
+				subn.indexOf("semester-" + sem) != -1 || 
+				subn.indexOf("semester - " + sem) != -1)
 					if (helper_text != "") {
-						if (subn.indexOf(helper_text) != -1) console.log(i, subn, yearn, "\n" + a[i].querySelector("a").href);
+						if (subn.indexOf(helper_text) != -1) 
+							console.log(i, subn, yearn, "\n" + a[i].querySelector("a").href);
 					} else
 						console.log(i, subn, yearn, "\n" + a[i].querySelector("a").href);
 			} else {
 				if (helper_text != "") {
-					if (subn.indexOf(helper_text) != -1) console.log(i, subn, yearn, "\n" + a[i].querySelector("a").href);
+					if (subn.indexOf(helper_text) != -1) 
+						console.log(i, subn, yearn, "\n" + a[i].querySelector("a").href);
 				} else
 					console.log(i, subn, yearn, "\n" + a[i].querySelector("a").href);
 			}
 		}
 	}
 }
+
 
 /* B.SC 2017-2020
 
