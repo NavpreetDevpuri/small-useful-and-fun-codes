f39769905dea7b2dae7b20cc81f51b25b5e8c760
Navpreet Singh Devpuri <navpreetdevpuri@gmail.com>
2020-12-17 00:39:48 +0530

    Add files via upload

---
+++ python/PaperDownlaoder.py
@@ -0,0 +1,66 @@
+from simplegmail import Gmail
+from datetime import datetime
+import time
+from PyPDF2 import PdfFileReader, PdfFileWriter
+import glob
+import os
+
+'''
+
+'''
+
+from_date_and_time = "16/12/2020 13:00:00"
+to_date_and_time = "16/12/2020 16:00:00"
+save_folder_name = "bsc sem 5"
+
+
+def _get_timestamp(date_and_time="16/12/2020 13:00:00"):
+    return int(datetime.strptime(date_and_time, "%d/%m/%Y %H:%M:%S").timestamp())
+
+
+class PaperDownloader:
+    def __init__(self, after_date_and_time, before_date_and_time, save_dir="output"):
+        self.gmail = Gmail()
+        self.save_dir = save_dir
+        self.messages = self.gmail.get_unread_inbox(
+            query=f'after:{_get_timestamp(after_date_and_time)} '
+                  f'before:{_get_timestamp(before_date_and_time)} '
+                  f'has:attachment')
+        os.makedirs(save_dir, exist_ok=True)
+        for message in self.messages:
+            print("From: " + message.sender)
+            if message.attachments:
+                for attm in message.attachments:
+                    if attm.filename.find(".pdf") != -1 or attm.filename.find(".PDF") != -1:
+                        file_path = os.path.join(save_dir, attm.filename)
+                        if os.path.exists(file_path):
+                            attm.filename = f'{attm.filename.replace(".pdf", "").replace(".PDF", "")}'
+                            attm.filename = f'{attm.filename}_{int(time.time())}.pdf'
+                        else:
+                            attm.filename = f'{attm.filename.replace(".pdf", "").replace(".PDF", "")}'
+                            attm.filename = f'{attm.filename}.pdf'
+                    file_path = os.path.join(save_dir, attm.filename)
+                    attm.save(file_path)
+                    print(f"   Downloaded: {file_path}")
+
+
+PaperDownloader(from_date_and_time, to_date_and_time, save_folder_name)
+
+
+def merge_pdfs(paths, output):
+    pdf_writer = PdfFileWriter()
+
+    for path in paths:
+        pdf_reader = PdfFileReader(path)
+        for page in range(pdf_reader.getNumPages()):
+            # Add each page to the writer object
+            pdf_writer.addPage(pdf_reader.getPage(page))
+
+    # Write out the merged PDF
+    with open(output, 'wb') as out:
+        pdf_writer.write(out)
+
+
+pdfList = glob.glob(os.path.join(save_folder_name, "*.pdf"))
+os.makedirs(save_folder_name + "_merged", exist_ok=True)
+merge_pdfs(pdfList, output=f'{os.path.join(save_folder_name + "_merged", save_folder_name + "_merged.pdf")}')
